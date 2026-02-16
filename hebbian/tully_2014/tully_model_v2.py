4b43045d1042046ce24b1fb8f7b0f6d0b5159142
FerdinandBujanowski <49587549+FerdinandBujanowski@users.noreply.github.com>
2026-02-16 16:51:08 +0100

    second Tully model + plotting

--- hebbian/tully_2014/tully_model_v2.py
+++ hebbian/tully_2014/tully_model_v2.py
@@ -1,25 +1,29 @@
+# Updated version of tully_model.py, this time with i and j traces saved at the Synapse level
+
 from brian2 import *
 import numpy as np
 import matplotlib.pyplot as plt
+
 from parameters import *
+from plot import trains
 
 defaultclock.dt = sim_dt
 
-N_hyper = 4
-N_mini = 4
+N_hyper = 8
+N_mini = 8
 N_total = N_hyper * N_mini
 
-min_num = 10e-4
+min_num = 10e-6
 
 eqs_rec = '''
-# POSTSYNAPTIC (j) traces
-dS/dt = -S/sim_dt : 1
-dZ/dt = (S/(f_max*t_spike) - Z + epsilon*second)/tau_z : 1
-dE/dt = (Z - E)/tau_e : 1
-dP/dt = (K*(E-P))/tau_p : 1
+# traces
+dS_j/dt = -S_j/sim_dt : 1
+dZ_j/dt = (S_j/(f_max*t_spike) - Z_j + epsilon*second)/tau_z : 1
+dE_j/dt = (Z_j - E_j)/tau_e : 1
+dP_j/dt = (K*(E_j-P_j))/tau_p : 1
 
 # bias
-beta = log(clip(P, min_num, inf)) : 1
+beta = log(clip(P_j, min_num, inf)) : 1
 I_beta = phi*beta : amp
 
 # on-switch
@@ -57,9 +61,9 @@ dE_i/dt = (Z_i - E_i)/tau_e : 1 (clock-driven)
 dP_i/dt = (K*(E_i-P_i))/tau_p : 1 (clock-driven)
 
 # BCPNN synapse
-dE_syn/dt = (Z_i*Z_post - E_syn)/tau_e : 1 (clock-driven)
+dE_syn/dt = (Z_i*Z_j_post - E_syn)/tau_e : 1 (clock-driven)
 dP_syn/dt = (K*(E_syn-P_syn))/tau_p : 1 (clock-driven)
-w = log(clip(P_syn, min_num, inf)/clip(P_i*P_post, min_num, inf)) : 1 (constant over dt)
+w = log(clip(P_syn, min_num, inf)/clip(P_i*P_j_post, min_num, inf)) : 1 (constant over dt)
 '''
 
 rec_syn_on_pre = '''
@@ -68,12 +72,12 @@ S_ex = 1
 S_inh = 1
 '''
 rec_syn_on_post = '''
-S_post = 1
+S_j_post = 1
 '''
 
 S_REC = Synapses(REC, REC, model=rec_syn_model, on_pre=rec_syn_on_pre, on_post=rec_syn_on_post, method='euler', delay=d)
 S_REC.connect(condition='i!=j')
-S_REC.b_ex[:] = 1 #'rand() > 0.2'
+S_REC.b_ex[:] = 'rand() > 0.2'
 # print(S_REC.b_ex, 0 in S_REC.b_ex)
 
 first_targets = []
@@ -82,24 +86,20 @@ for source, target in zip(S_REC.i, S_REC.j):
         first_targets.append(target)
 
 spikemon = SpikeMonitor(REC)
-statemon = StateMonitor(REC, ['V', 'S', 'Z', 'E', 'P', 'beta', 'g_ex', 'g_inh', 'I_beta', 'I_on'], record=[0, 1])
-synmon = StateMonitor(S_REC, ['w', 'P_syn', 'E_syn'], record=S_REC[0, 8])
+statemon = StateMonitor(REC, ['V', 'S_j', 'Z_j', 'E_j', 'P_j', 'beta', 'g_ex', 'g_inh', 'I_beta', 'I_on'], record=[0, 1])
+synmon = StateMonitor(S_REC, ['w', 'P_syn', 'E_syn'], record=True)
 
 tfinal = 1000 * ms
 run(tfinal)
 
+
 fig, (ax, ax_voltage) = plt.subplots(2, 1, sharex=True,
-                                     gridspec_kw={'height_ratios': (3, 1)})
+                                     gridspec_kw={'height_ratios': (1, 4)})
 
-ax.scatter(spikemon.t / ms, spikemon.i[:], marker="_", color="k", s=10)
-ax.set_xlim(0, tfinal / ms)
-ax.set_ylim(0, N_total)
-ax.set_ylabel("neuron number")
-ax.set_yticks(np.arange(0, N_total, 100))
-ax.spines['right'].set_visible(False)
-ax.spines['top'].set_visible(False)
+# trains.get_full_train(ax, spikemon, N_total)
+trains.compare_two_trains(ax, spikemon, 0, 7)
 
-ax_voltage.plot(statemon.t / ms, statemon.E[0], # np.clip(statemon.v[0], -np.inf, 30),
+ax_voltage.plot(statemon.t / ms, statemon.E_j[0], # np.clip(statemon.v[0], -np.inf, 30),
                color='k')
 
 ax_voltage.set_xticks(np.arange(0, tfinal / ms, 100))
---
+++ test.py
@@ -0,0 +1,16 @@
+from brian2 import *
+from hebbian.tully_2014.parameters import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(epsilon)
+
+V_space = np.linspace(-70, 0, 1000) * mV
+
+def get_momentary_change(V, dt=sim_dt):
+    return (g_L*(V-E_L)/-C_m)/(mV/ms)
+
+plt.plot(V_space/mV, get_momentary_change(V_space))
+plt.xlabel('V (mV)')
+plt.ylabel('dV/dt (mV/ms)')
+plt.show()
