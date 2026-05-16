428452046b4dbafcb9fe6b93f785cf8fdc3d1b72
FerdinandBujanowski <49587549+FerdinandBujanowski@users.noreply.github.com>
2026-05-16 17:04:11 +0200

    trained B_5_2

--- brian_bcpnn/models/bujanowski_2026/test_distortion.py
+++ brian_bcpnn/models/bujanowski_2026/test_distortion.py
@@ -26,7 +26,7 @@ t_stim = 50 * ms
 t_end = 0 * ms
 
 SERIES = 'B'
-BATCH = '5_1'
+BATCH = '5_2'
 
 fp = f'./data/random_patterns/weights_series_{SERIES}_{BATCH}.data'
 pattern_list = stils.patterns_from_txt(f'20_random_patterns/tests_{SERIES}/patterns_{SERIES}.txt')
@@ -51,7 +51,7 @@ namespace['p_c_inter_hc'] = 1.0 # here it doesn't matter - it needs to be correc
 namespace['p_c_intra_mc'] = 1.0
 
 namespace['G_PB_factor'] = 6
-namespace['gain_factor'] = 1
+namespace['gain_factor'] = 2.2
 
 N_runs = 200 # make it 200 after testing
 for _ in tqdm(range(N_runs)):
