0ed2e33b8a1f92e46a7eda95dea6ccbcb707256b
FerdinandBujanowski <49587549+FerdinandBujanowski@users.noreply.github.com>
2026-05-16 16:47:19 +0200

    trained B_5_1

--- brian_bcpnn/models/bujanowski_2026/train_random_patterns.py
+++ brian_bcpnn/models/bujanowski_2026/train_random_patterns.py
@@ -11,14 +11,14 @@ import brian_bcpnn.utils.synapse_utils as syls
 
 N_H = 9
 N_M = 9
-N_pyr = 10
+N_pyr = 5
 N_BA = 2
 N_batches = 1
 
 N_P = 20 # amount of random patterns
 
 SERIES = 'B'
-BATCH = '10_3'
+BATCH = '5_2'
 
 # UPDATE FIEBIG NAMESPACE PARAMETERS
 namespace = fiebig_namespace
@@ -27,9 +27,9 @@ namespace['b_recurrence'] = 0 # TURN OFF RECURRENCE
 
 # parameter tests
 namespace['p_c_inter_hc'] = 1.0
-namespace['p_c_intra_mc'] = 0.75
-namespace['G_PB_factor'] = 3
-namespace['gain_factor'] = 1
+namespace['p_c_intra_mc'] = 1.0
+namespace['G_PB_factor'] = 6
+namespace['gain_factor'] = 2.2
 
 model = TwoSynTypeNetwork(N_H, N_M, N_pyr=N_pyr, N_BA=N_BA, namespace=namespace, eqs=fiebig_equations)
 
